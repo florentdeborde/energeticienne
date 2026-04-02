779ababc120ca240147acb726bcffaced8689961
florentdeborde <florentdeborde@outlook.com>
2026-04-02 08:02:29 +0200

    fix: solve header shifting and horizontal overflow issues on mobile

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import './Footer.css';
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { handleLogoNavigation } from "../../utils/navigationUtils";
 
-const Footer = ({t, menu, logoId = "home", legalInformationId = "mentions-legales", logoSrc, iframeSrc }) => {
+const Footer = ({ t, menu, logoId = "home", legalInformationId = "mentions-legales", logoSrc, iframeSrc }) => {
   const location = useLocation();
   const navigate = useNavigate();
 
@@ -15,19 +15,19 @@ const Footer = ({t, menu, logoId = "home", legalInformationId = "mentions-legale
   const email = t(`footer.email`);
   const address = t(`footer.address`);
   const phone = t(`footer.phone`);
-  
+
   return (
     <footer className="footer">
       <div className="footer-container">
-        
+
         {/* Left side */}
         <div className="footer-left">
           <div className="footer-left-1">
             {/* Logo */}
             <Link to={logoItem.path} onClick={(e) => handleLogoNavigation(e, location.pathname, navigate, logoItem.path)} style={{ cursor: "pointer" }}>
-              <img src={logoSrc} style={{ maxHeight: "200px" }} alt="Logo" className="footer-logo"/>
+              <img src={logoSrc} style={{ maxHeight: "200px" }} alt="Logo" className="footer-logo" />
             </Link>
-            
+
             {/* Domain & legal */}
             <div className="domain-and-legal">
               {legalInformation && <Link className="a-footer" to={legalInformationItem.path}>{legalInformation}</Link>}
@@ -50,8 +50,8 @@ const Footer = ({t, menu, logoId = "home", legalInformationId = "mentions-legale
         {/* Right side */}
         <div className="footer-map">
           {/* Maps */}
-          <iframe title="Location" src={iframeSrc} width="400" height="300" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            
+          <iframe title="Location" src={iframeSrc} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
           {/* Address & phone */}
           <div className="address-and-phone">
             {address &&
@@ -59,13 +59,13 @@ const Footer = ({t, menu, logoId = "home", legalInformationId = "mentions-legale
                 {address.split("\n").map((line, index) => (<div key={index}>{line}</div>))}
               </div>
             }
-            
+
             {phone &&
-              <div style={{display: "flex", justifyContent: "center"}}>
-                  <a className="a-footer" href={"tel:" + phone}>
-                    <img src="/images/core/social_network/icons8-telephone-32.png" alt="tel" className="social-icon" style={{ verticalAlign: "middle", marginRight: "8px", maxWidth: "32px" }} />
-                    {phone}
-                  </a>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <a className="a-footer" href={"tel:" + phone}>
+                  <img src="/images/core/social_network/icons8-telephone-32.png" alt="tel" className="social-icon" style={{ verticalAlign: "middle", marginRight: "8px", maxWidth: "32px" }} />
+                  {phone}
+                </a>
               </div>
             }
           </div>
